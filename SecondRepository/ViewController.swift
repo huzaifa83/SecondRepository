8017dd41ce0c2568f8ff2b81910f541ca87a9e1f
Huzaifa Gadiwala <huzaifagadiwala@gmail.com>
2017-12-05 23:54:14 -0800

    Initial commit

---
+++ SecondRepository/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondRepository
+//
+//  Created by Huzaifa Gadiwala on 5/12/2017.
+//  Copyright © 2017 HuzaifaGadiwala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
